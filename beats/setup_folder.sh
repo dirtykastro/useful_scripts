10aaf58f2754fd32ad8189151c6fe33a99c0b83b
Juan Castro <info@jempe.org>
2021-05-08 12:00:02 -0500

    beats script

---
+++ beats/setup_folder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir AmazonPolly
+mkdir Ardour
+mkdir -p Master/Mixed
+mkdir -p Master/Stems
+mkdir -p Master/Mastered
+mkdir Reason
+mkdir -p Video/Render
+mkdir -p Video/StoryBlocks
+mkdir -p Video/Badge
+
+echo ".DS_Store" >> .gitignore
+echo "Video/Badge" >> .gitignore
+
