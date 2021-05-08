3f7f899bd1b024071fcf7e8002ac5fbe2ed4ac1e
Juan Castro <info@jempe.org>
2021-05-08 13:36:55 -0500

    create only when doesn't exist

--- beats/setup_folder.sh
+++ beats/setup_folder.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-mkdir AmazonPolly
-mkdir Ardour
-mkdir -p Master/Mixed
-mkdir -p Master/Stems
-mkdir -p Master/Mastered
-mkdir Reason
-mkdir -p Video/Render
-mkdir -p Video/StoryBlocks
-mkdir -p Video/Badge
+FOLDERSLIST="AmazonPolly Ardour Master/Mixed Master/Stems Master/Mastered Reason Video/Render Video/StoryBlocks Video/Badge"
 
-echo ".DS_Store" >> .gitignore
-echo "Video/Badge" >> .gitignore
+for FOLDER in $FOLDERSLIST; do
+	if [ -d $FOLDER ];
+	then
+		echo "$FOLDER already exists"
+	else
+		mkdir -p $FOLDER
+	fi
+done
 
+
+# Define a string variable with a value
+GITIGNORELIST=".DS_Store Video/Badge"
+
+GITIGNOREFILE=".gitignore"
+
+for IGNORE in $GITIGNORELIST; do
+	if [ -f $GITIGNOREFILE ];
+	then
+		if grep -q "$IGNORE" "$GITIGNOREFILE";
+		then
+			echo "$IGNORE already in $GITIGNOREFILE"
+		else
+			echo "$IGNORE" >> $GITIGNOREFILE
+		fi	
+
+	else
+		echo "$GITIGNOREFILE doesn't exist"
+	fi
+done
